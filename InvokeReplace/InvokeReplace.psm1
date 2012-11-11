11dcf384521b24993d7e597f5c1244a95fa2d486
Doug Finke <finked@hotmail.com>
2012-11-10 20:03:29 -0500

    Added help message

--- InvokeReplace/InvokeReplace.psm1
+++ InvokeReplace/InvokeReplace.psm1
@@ -1,4 +1,25 @@
 function Invoke-Replace {
+    <#
+        .Synopsis
+            PowerShell Inplace Editing 
+        .Description
+            
+        .Example
+            Invoke-Replace old new
+            In all the files in the current directory, replace the string "old" with the string "new"
+
+        .Example
+            Invoke-Replace old new *.txt
+            In all files with the extension '.txt' in the current directory, replace the string "old" with the string "new"
+
+        .Example
+            Invoke-Replace old new *.txt .bak
+            In all files with the extension '.txt' in the current directory, create a copy and append the extension .bak and then replace the string "old" with the string "new"
+
+        .Example
+            Invoke-Replace "(?<=d)," ""
+            In all the files in the current directory, use the regex "(?<=d)," to remove commas that immediately follow a digit
+    #>
     param(
         $Match,
         $Replace,
